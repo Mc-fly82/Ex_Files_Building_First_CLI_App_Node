339e6e8b89469efdc07649e41ba075c707b9f3f2
Mc-fly82 <flavius_marc@yahoo.fr>
2022-04-01 01:54:06 +0200

    add receive the github token logic

--- app/lib/github_credentials.js
+++ app/lib/github_credentials.js
@@ -1,4 +1,25 @@
+const octokit = require('@octokit/rest')();
 const Configstore = require('configstore');
-const pkg = require('../package.json');
+const _ = require('lodash');
 
+const pkg = require('../package.json');
 const conf = new Configstore(pkg.name);
+
+module.exports = {
+    getInstance: () => {
+        return octokit
+    },
+    //if deprecation notice check out https://octokit.github.io
+    gitHubAuth: (token) => {
+        octokit.authenticate({
+            type: "oauth",
+            token,
+        })
+    },
+    getStoredGitHubToken: () => {
+        return conf.get('github_credentials.token')
+    },
+    setGitHubCredentials: async () => {
+       
+    },
+}
