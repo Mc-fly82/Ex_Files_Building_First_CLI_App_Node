3e094404dd8f9b04184889f0a38040db85852a64
Mc-fly82 <flavius_marc@yahoo.fr>
2022-04-01 01:37:07 +0200

    add valide user input logic

--- app/lib/inquire.js
+++ app/lib/inquire.js
@@ -1,5 +1,24 @@
-const inquire = require('inquirer');
+const inquirer = require('inquirer');
 const minimist = require('minimist');
 const files = require('./files');
 
+module.exports = {
+    askGitHubCredentials: () => {
+        const questions = [
+            {
+                name: "username",
+                type:"input",
+                message: "Enter your github username or e-mail address:",
+                validate: function (value) {
+                    if(value.length) {
+                        return true
+                    } else {
+                        return "Please enter your GitHub username or e-mail address:";
 
+                    }
+                },
+            }
+        ]
+        return inquirer.prompts(questions)
+    },
+}
