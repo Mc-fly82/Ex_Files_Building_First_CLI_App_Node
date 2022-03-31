73833347db6e436fb28e033cefe13999fdd1b4cf
Mc-fly82 <flavius_marc@yahoo.fr>
2022-04-01 01:15:50 +0200

    create the user interface

--- app/index.js
+++ app/index.js
@@ -1,3 +1,21 @@
+const musette = require('commander');
+
 const chalk = require('chalk');
 const figlet = require('figlet');
 const clear = require('clear')
+const files = require('./lib/files');
+
+musette
+	.command('init')
+	.description('Draw app header')
+	.action(() => {
+		clear();
+		console.info(chalk.magenta(figlet.textSync("musette",{horizontalLayout: "full"})))
+	})
+
+
+musette.parse(process.argv);
+if(!musette.args.length) {
+	musette.help();
+}
+
